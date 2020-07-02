1f13e57933e7b79f40cc2a4e9025856e4c35b1e8
thomasin <thomasinabraham@me.com>
2020-07-02 21:04:47 +1200

    Update code to add support for returning error messages

---
+++ lib/event_responder.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Generate event responses
+# Only used for the Handler return value
+class EventResponder
+  def initialize(res)
+    @res = res
+  end
+
+  def success
+    @res.status = 200
+    self
+  end
+
+  def error(code)
+    @res.status = code
+    self
+  end
+
+  def ephemeral(text)
+    hsh = {
+      response: 'ephemeral',
+      text: text
+    }
+
+    send_json(hsh)
+  end
+
+  private
+
+  def send_json(response_body)
+    @res['Content-type'] = 'application/json'
+    res.body = response_body.to_json
+  end
+end
