e01cd34312a794ee134a1ad479ce03cfa35d5645
thomasin <thomasinabraham@me.com>
2020-07-02 18:40:56 +1200

    Respond with a form encoded string

--- api/shuffle.rb
+++ api/shuffle.rb
@@ -11,21 +11,16 @@ Handler = proc do |req, res|
 
   request_body = decode_body(req.body)
 
-  puts request_body.to_s
-  puts request_body['channel_id']
-
   conversation_members = slack.web_api.conversations_members(
     channel: request_body['channel_id']
   )
 
-  puts conversation_members
-
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
-  res.body = {
-    response_type: 'ephemeral',
-    text: 'shuffling (:'
-  }
+  res.body = URI.encode_www_form({
+    'response_type' => 'ephemeral',
+    'text' => 'shuffling (:'
+  })
 end
 
 def verify_request!(slack, req)
